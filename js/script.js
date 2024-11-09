0aa12ad699a23dca877c18d227464569afc3592b
NordrumDesigns <nordrum.chris@gmail.com>
2024-11-09 16:23:03 -0700

    feat: change about page

--- js/script.js
+++ js/script.js
@@ -1,1 +1,20 @@
 console.log('Hello, world');
+
+// greetings
+const d = new Date();
+const hrs = d.getHours();
+
+var greet;
+
+if (hrs < 12)
+  greet = 'Good morning,';
+else if (hrs >= 12 && hrs <= 17)
+  greet = 'Good afternoon,';
+else if (hrs >= 17 && hrs <= 24)
+  greet = 'Good evening,';
+
+document.getElementById('greetings').innerHTML = greet;
+
+// footerYear
+const fYear = document.getElementById('footerYear');
+fYear.textContent = d.getFullYear();
